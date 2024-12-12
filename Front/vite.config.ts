40b692db96fabcaa05f9db2a344fd52306c53417
chimal <carbajalchimalisaac0@gmail.com>
2024-12-12 09:37:36 -0600

    Mas cambios para el host otra vez

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.PORT) || 3000,
+    host: true,
+    strictPort: true
+  },
+  preview: {
+    port: Number(process.env.PORT) || 3000,
+    host: true,
+    strictPort: true
+  },
   plugins: [react()],
   resolve: {
     alias: [{find: "@", replacement: path.resolve(__dirname, "src")}]
