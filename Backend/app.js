7b8acc1bbec9eaf3beca246da78c6160029cec1b
chimal <carbajalchimalisaac0@gmail.com>
2024-12-12 19:10:59 -0600

    Esta es la buena (espero-13)

--- Backend/app.js
+++ Backend/app.js
@@ -15,8 +15,14 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://ecoblastic-back.onrender.com',
-    credentials: true
+    origin: [
+    'https://ecoblastic-back.onrender.com',
+    'http://localhost:5173',
+    'http://localhost:3000',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));app.use(helmet());
 app.use(cookieParser());
 app.use('/api', data_sensorsRoutes);
